8d3695bf322da4f5e6a6d2d45f323fd1b16e59e8
Shivansh Saxena <44111318+Signature1-hitman@users.noreply.github.com>
2020-06-10 02:17:01 +0530

    Add files via upload

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import '@styles/Footer.css';
+
+const Footer = () => (
+  <div>
+     <hr className="xoc" /> 
+  <div className="cs">
+    
+    <table>
+  
+  <tr>
+    <td>About</td>
+    <td>HELP</td>
+    <td>POLICY</td>
+    <td>SOCIAL</td>
+    <td>Mail us</td>
+    
+  </tr>
+  <tr>
+    <td>Contact us</td>
+    <td>Payments</td>
+    <td>Return Policy</td>
+    <td>Facebook</td>
+    <td>online basket.com</td>
+  </tr>
+  <tr>
+    <td>About us</td>
+    <td>Shipping</td>
+    <td>Terms of Use</td>
+    <td>Twitter</td>
+    <td>Or contact us on</td>
+  </tr>
+  <tr>
+    <td>Careers</td>
+    <td>Cancellation </td>
+    <td>Security</td>
+    <td>Youtube</td>
+    <td>1234567890</td>
+  </tr>
+  <tr>
+    <td>Our stories</td>
+    <td>Return</td>
+    <td>Privacy</td>
+    <td>   </td>
+  </tr>
+  <tr>
+    <td>Press</td>
+    <td>FAQ</td>
+    <td>SiteMap</td>
+    <td></td>
+  </tr>
+</table>
+    
+    
+  </div>
+  <div className="footer"><p>Online Basket Limited &copy; 2020</p></div>
+  </div>
+);
+
+export default Footer;
